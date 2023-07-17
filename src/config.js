f7b17ae51d9b040b5e4b4a555eaa5c4ba55886df
unknown <katheri18valero@gmail.com>
2023-07-17 16:35:58 -0400

    firstcommit

---
+++ src/config.js
@@ -0,0 +1,17 @@
+const defaultAvatar = 'https://firebasestorage.googleapis.com/v0/b/expo-boilerplate-v2.appspot.com/o/icon.png?alt=media&token=505f30e6-6702-410a-84e7-f66b3ff60daf'
+
+const firebaseKey = {
+  apiKey: "AIzaSyCo6HFjTb1z2Ij1Ub6ylLz5zCgu5Vpq-wc",
+  authDomain: "cdoprueba.firebaseapp.com",
+  projectId: "cdoprueba",
+  storageBucket: "cdoprueba.appspot.com",
+  messagingSenderId: "819566585445",
+  appId: "1:819566585445:web:d4a0b01b2acba9334fc88c",
+  measurementId: "G-BZ87VPRBQL"
+}
+
+const eulaLink = 'https://github.com/kiyohken2000/ReactNative-Expo-Firebase-Boilerplate-v2'
+
+const expoProjectId = '1650611b-a5b8-4420-9656-60c06429edd4'
+
+export { defaultAvatar, firebaseKey, eulaLink, expoProjectId }
